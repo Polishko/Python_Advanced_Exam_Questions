1a217422aabd4d0aa8de356477637955b720d229
Niya Mateeva (Nalan Bilgin) <119063181+Polishko@users.noreply.github.com>
2023-06-11 16:02:20 +0300

    Added solutions
    Added solutions to questions

---
+++ June_2020_Retake/num_search.py
@@ -0,0 +1,15 @@
+def numbers_searching(*args):
+    nums = list(args)
+    all_range = set(range(min(nums), max(nums) + 1))
+    missing = sorted(list(all_range.difference(nums)))
+
+    duplicates = {}
+    for num in nums:
+        if num not in duplicates:
+            duplicates[num] = 0
+        duplicates[num] += 1
+
+    return [missing[-1], sorted([num for num in duplicates if duplicates[num] > 1])]
+
+
+print(numbers_searching(3, 6, 5, 5, 9, 10, 7, 8, 7, 9))
